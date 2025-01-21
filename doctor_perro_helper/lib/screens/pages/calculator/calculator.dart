e4872e99c16c94816cd5bb886e1c6ccbbc1bc19a
David Space <genarodavid@gmail.com>
2025-01-21 16:30:59 -0400

    added buttons and styled them

--- doctor_perro_helper/lib/screens/pages/calculator/calculator.dart
+++ doctor_perro_helper/lib/screens/pages/calculator/calculator.dart
@@ -13,117 +13,6 @@ class DolarCalculator extends StatefulWidget {
 class _DolarCalculatorState extends State<DolarCalculator> {
   @override
   Widget build(BuildContext context) {
-    final Color specialButtonColor =
-        Theme.of(context).colorScheme.secondary.withAlpha(150);
-    final Color specialButtonTextColor =
-        Theme.of(context).colorScheme.onSecondary;
-
-    final Color normalButtonColor =
-        Theme.of(context).colorScheme.surfaceContainerHighest.withAlpha(50);
-    final Color normalButtonTextColor = Theme.of(context).colorScheme.onSurface;
-
-    final List<CalculatorButtonData> buttons = [
-      CalculatorButtonData(
-        color: Colors.lightGreenAccent,
-        text: "C",
-        textColor: Colors.black,
-      ),
-      CalculatorButtonData(
-        color: Colors.redAccent,
-        text: "DEL",
-        textColor: Colors.white,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "%",
-        textColor: specialButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "/",
-        textColor: specialButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "9",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "8",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "7",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "x",
-        textColor: specialButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "6",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "5",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "4",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "-",
-        textColor: specialButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "3",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "2",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "1",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "+",
-        textColor: specialButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "x60",
-        textColor: specialButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "0",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: normalButtonColor,
-        text: "Copy",
-        textColor: normalButtonTextColor,
-      ),
-      CalculatorButtonData(
-        color: specialButtonColor,
-        text: "=",
-        textColor: specialButtonTextColor,
-      )
-    ];
     return Scaffold(
       body: Center(
         child: Column(
@@ -145,6 +34,114 @@ class _DolarCalculatorState extends State<DolarCalculator> {
   }
 
   GridView gridButtons() {
+    final Color specialButtonColor = Theme.of(context).colorScheme.secondary;
+    final Color specialButtonTextColor =
+        Theme.of(context).colorScheme.onSecondary;
+
+    final Color normalButtonColor =
+        Theme.of(context).colorScheme.surfaceContainerHighest;
+    final Color normalButtonTextColor = Theme.of(context).colorScheme.onSurface;
+
+    final List<CalculatorButtonData> buttons = [
+      /* CalculatorButtonData(
+        color: Colors.lightGreenAccent,
+        text: "C",
+        textColor: Colors.black,
+      ),
+      CalculatorButtonData(
+        color: Colors.redAccent,
+        text: "DEL",
+        textColor: Colors.white,
+      ),
+      CalculatorButtonData(
+        color: specialButtonColor,
+        text: "%",
+        textColor: specialButtonTextColor,
+      ), */
+
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "9",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "8",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "7",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: specialButtonColor,
+        text: "/",
+        textColor: specialButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "6",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "5",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "4",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: specialButtonColor,
+        text: "x",
+        textColor: specialButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "3",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "2",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "1",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: specialButtonColor,
+        text: "—",
+        textColor: specialButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: specialButtonColor,
+        text: "Copy",
+        textColor: specialButtonTextColor,
+        icon: Icons.copy,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: "0",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: normalButtonColor,
+        text: ",",
+        textColor: normalButtonTextColor,
+      ),
+      CalculatorButtonData(
+        color: specialButtonColor,
+        text: "+",
+        textColor: specialButtonTextColor,
+      ),
+    ];
+
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 4,
@@ -157,12 +154,11 @@ class _DolarCalculatorState extends State<DolarCalculator> {
         top: Sizes().xxxl * 2,
       ),
       physics: const NeverScrollableScrollPhysics(),
-      itemCount: 16,
+      itemCount: buttons.length,
       itemBuilder: (BuildContext context, int index) => CalculatorButton(
-        buttonData: CalculatorButtonData(
-            color: Colors.amber, text: "$index", textColor: Colors.black),
+        buttonData: buttons[index],
         onTap: () {
-          log(index.toString());
+          log(buttons[index].text);
         },
       ),
     );
@@ -170,19 +166,24 @@ class _DolarCalculatorState extends State<DolarCalculator> {
 }
 
 class CalculatorButtonData {
-  CalculatorButtonData({
-    required this.color,
-    required this.text,
-    required this.textColor,
-  });
+  CalculatorButtonData(
+      {required this.color,
+      required this.text,
+      required this.textColor,
+      this.icon});
   Color color;
   Color textColor;
   String text;
+
+  IconData? icon;
 }
 
 class CalculatorButton extends StatelessWidget {
-  const CalculatorButton(
-      {super.key, required this.buttonData, required this.onTap});
+  const CalculatorButton({
+    super.key,
+    required this.buttonData,
+    required this.onTap,
+  });
 
   final CalculatorButtonData buttonData;
   final void Function() onTap;
@@ -196,10 +197,48 @@ class CalculatorButton extends StatelessWidget {
         child: Container(
           color: buttonData.color,
           child: Center(
-            child: Text(
-              buttonData.text,
-              style: TextStyle(color: buttonData.textColor),
-            ),
+              child: buttonData.icon != null
+                  ? Icon(buttonData.icon,
+                      size: Theme.of(context).textTheme.headlineSmall?.fontSize,
+                      color: buttonData.textColor)
+                  : Text(
+                      buttonData.text,
+                      style: TextStyle(
+                        color: buttonData.textColor,
+                        fontSize:
+                            Theme.of(context).textTheme.headlineSmall?.fontSize,
+                      ),
+                    )),
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorButtonIcon extends StatelessWidget {
+  const CalculatorButtonIcon({
+    super.key,
+    required this.buttonData,
+    required this.onTap,
+    required this.icon,
+  });
+
+  final CalculatorButtonData buttonData;
+  final IconData icon;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(Sizes().roundedSmall),
+        child: Container(
+          color: buttonData.color,
+          child: Center(
+            child: Icon(icon,
+                size: Theme.of(context).textTheme.headlineSmall?.fontSize,
+                color: buttonData.textColor),
           ),
         ),
       ),
