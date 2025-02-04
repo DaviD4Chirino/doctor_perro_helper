2aa85e7987ad7cfd31018a64767fc49f7b05f792
David Space <genarodavid@gmail.com>
2025-02-03 20:01:14 -0400

    added local price under menu_list_tile

--- doctor_perro_helper/lib/widgets/menu_list_item.dart
+++ doctor_perro_helper/lib/widgets/menu_list_item.dart
@@ -1,9 +1,11 @@
 import 'package:doctor_perro_helper/config/border_size.dart';
 import 'package:doctor_perro_helper/models/plate.dart';
+import 'package:doctor_perro_helper/models/providers/settings.dart';
 import 'package:doctor_perro_helper/utils/string_math.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MenuListItem extends StatelessWidget {
+class MenuListItem extends ConsumerWidget {
   const MenuListItem({
     super.key,
     required this.plate,
@@ -11,7 +13,8 @@ class MenuListItem extends StatelessWidget {
   final Plate plate;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ThemeData theme = Theme.of(context);
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(Sizes().roundedSmall),
@@ -48,12 +51,23 @@ class MenuListItem extends StatelessWidget {
         ),
         // This will become a checkmark when the user taps on it
         leading: const Icon(Icons.fastfood),
-        trailing: Text(
-          "${removePaddingZero(plate.price.toString())}\$",
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Theme.of(context).colorScheme.primary,
-          ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${removePaddingZero(plate.price.toString())}\$",
+              style: TextStyle(
+                fontSize: theme.textTheme.titleMedium?.fontSize,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            Text(
+              "${removePaddingZero(ref.read(dolarPriceNotifierProvider.notifier).calculate(plate.price).toString())}bs",
+              style: TextStyle(
+                fontSize: theme.textTheme.labelSmall?.fontSize,
+              ),
+            ),
+          ],
         ),
       ),
     );
