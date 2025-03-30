debaf3937bec052bd090e2bea4c422bcd63f5621
David Space <genarodavid@gmail.com>
2025-03-30 16:36:48 -0400

    changed the version

--- doctor_perro_helper/build_and_rename.bat
+++ doctor_perro_helper/build_and_rename.bat
@@ -1,7 +1,6 @@
 
-set VERSION=0.4.2
+set VERSION=0.5.0
 :: Build the APK
-flutter build apk
 
 rename .\build\app\outputs\flutter-apk\app-release.apk dr-perro-helper-%VERSION%.apk
 rename .\build\app\outputs\flutter-apk\app-release.apk.sha1 dr-perro-helper-%VERSION%.apk.sha1
