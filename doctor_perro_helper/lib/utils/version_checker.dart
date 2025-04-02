73e55a556ef6a322e26e96c0d22ccdf093ae0095
David Space <genarodavid@gmail.com>
2025-04-02 01:16:42 -0400

    added check_for_updates and getLatestAppVersion

--- doctor_perro_helper/lib/utils/version_checker.dart
+++ doctor_perro_helper/lib/utils/version_checker.dart
@@ -1,21 +1,13 @@
-import 'dart:convert';
-
+import 'package:doctor_perro_helper/utils/get_latest_app_version.dart';
 import 'package:doctor_perro_helper/utils/version_to_string.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
 import 'package:package_info_plus/package_info_plus.dart';
 
 // * https://api.github.com/repos/DaviD4Chirino/doctor_perro_helper/releases/latest
 
 Future<bool> updateAvailable() async {
-  final httpPackageUrl = Uri.https(
-    "api.github.com",
-    "/repos/DaviD4Chirino/doctor_perro_helper/releases/latest",
-  );
-
   try {
-    final data = await http.read(httpPackageUrl);
-    final map = JsonDecoder().convert(data);
+    final map = await getLatestAppVersion();
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     int currentVersion = versionStringToInt(packageInfo.version);
     int tag = versionStringToInt(map["tag_name"]);
